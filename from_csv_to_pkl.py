8f55cfbb9b20152628e09ee322ed2e9a5e8806a7
Xiaorui Liu <781124785@qq.com>
2018-09-30 23:04:47 -0500

    Add files via upload

--- from_csv_to_pkl.py
+++ from_csv_to_pkl.py
@@ -7,48 +7,6 @@ import numpy as np
 import time
 
 
-# def pre_process_data(data_temp):
-#     ddata = np.zeros((1, 201)) # build 2*100 new array
-#
-#     # from DataFrame to array
-#     data_temp = np.array(data_temp)
-#     # acquire the index of the sorted values
-#     sorted_index = np.argsort(data_temp[0, 1:])
-#
-#     ddata[0, 0] = data_temp[0, 1]
-#     for i in range(1, 101):  # i=1~100
-#         ddata[0, i] = sorted_index[40183 - i]+1
-#         ddata[0, i + 100] = data_temp[0, sorted_index[40183 - i]+1]
-#
-#     return ddata
-#
-#
-# # 在程序所在目录下创建一个test.data文件
-# start_time = time.time()
-# pklfile = open("./Dataset/test.pkl", "ab")
-# rootDir = r'C:\Users\King\Desktop\tensorflow\torch_model_2\Dataset\Traindataset'
-#
-# for lists in os.listdir(rootDir):
-#      # print(lists)
-#      str1 = os.path.join(rootDir, lists)
-#      print("present dir:" + lists)
-#      for root, dirs, files in os.walk(str1):
-#         # print(files)
-#         for file in files:
-#             str2 = os.path.join(str1, file)
-#
-#             print("  ----start to read csv file:" + file)
-#             data_temp = pd.read_csv(str2, header=None)
-#
-#             data = pre_process_data(data_temp)
-#             pickle.dump(data, pklfile)
-#             print("      Data has been saved into pkl dataset")
-#
-# print("ALL over")
-# pklfile.close()
-# print((time.time()-start_time))
-
-
 def pre_process_data(data_temp):
     ddata = np.zeros((2, 100))
 
@@ -66,7 +24,6 @@ def pre_process_data(data_temp):
     return list_temp
 
 
-# 在程序所在目录下创建一个test.data文件
 rootDir = r'C:\Users\King\Desktop\tensorflow\torch_model_2\Dataset\Traindataset'
 Total_data_list = []
 start_time = time.time()
